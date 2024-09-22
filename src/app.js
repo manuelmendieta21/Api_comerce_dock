c5416472a96f65bab2ca52b8cba561f35744ee77
manuelmendieta21 <46815715+manuelmendieta21@users.noreply.github.com>
2024-09-22 15:03:26 -0500

    configure docker package

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
-const express = requiere('express');
+const express = require('express');
+const morgan= require('morgan');
 const app = express();
+
+
+/*incluimos morgan con la libreria dev existen combined, common, dev, short, tiny*/
+app.use(morgan('dev'));
+
+app.use(require('./routes/index'));
 
 module.exports =app;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-const app = requiere('./app');
+const app = require('./app');
+
+
+async function main(){
+    await app.listen(3000);
+    console.log('Server is running on port 3000');
+}
+
+main();     
